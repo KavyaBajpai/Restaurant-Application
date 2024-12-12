46d41eba38fa2b274fcf1bf2a5171cae42548b22
KavyaBajpai <kavyabajpai05@gmail.com>
2024-12-13 00:30:24 +0530

    added controller for reservation

---
+++ Backend/controllers/reservation.js
@@ -0,0 +1,34 @@
+import ErrorHandler from "../error/error.js";
+import { Reservation } from "../models/reservationSchema.js"
+
+export const sendReservation = async (req, res, next) => {
+
+    const { firstName, lastName, email, phone, time, date } = req.body;
+    if( !firstName || !lastName || !email || !phone || !time || !date )
+    {
+        return next( new ErrorHandler("Please fill all details.", 400) )
+    }
+
+    try
+    {
+        await Reservation.create( { firstName, lastName, email, phone, time, date } );
+        res.status(200).json(
+            {
+                success: true,
+                message: "Reservation made successfully."
+            }
+        );
+    }
+
+    catch ( error )
+    {
+        if(error.name === "ValidationError")
+        {
+            const validationErrors = Object.values( error.errors ).map( 
+                (err) => err.message
+             );
+
+            return  next( new ErrorHandler(validationErrors.join(","), 400 ))
+        }
+    }
+}
