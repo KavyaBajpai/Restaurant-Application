763d855bb799df0412d4af9e5790cf48a9ce6b03
KavyaBajpai <kavyabajpai05@gmail.com>
2024-12-12 22:38:25 +0530

    added routes folder

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { dbConnect } from "./db_connect/db_connect.js";
 import { ErrorMiddleware } from "./error/error.js";
+import reservationRouter from "./routes/reservationRoute.js"
 
 
 const app = express()
@@ -17,6 +18,7 @@ app.use( cors({
 
 app.use( express.json )
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use("/api/v1/reservation", reservationRouter)
 
 dbConnect();
 
