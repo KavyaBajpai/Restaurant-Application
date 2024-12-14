2ea0e2101044e3f6918716b0420eced1f655327b
KavyaBajpai <kavyabajpai05@gmail.com>
2024-12-14 19:12:41 +0530

    added route configs and restApi file

--- Frontend/restaurant-application/src/App.jsx
+++ Frontend/restaurant-application/src/App.jsx
@@ -1,16 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
+import NotFound from './pages/NotFound'
+import Home from './pages/Home'
+import Success from './pages/Success'
 
-function App() {
+const App = () => {
  
 
-  return (
-    <>
-      <div>
-
-      </div>
-    </>
-  )
+  return <Router>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/success" element={<Success/>} />
+      <Route path="*" element={<NotFound/>} />
+    </Routes>
+  </Router>
 }
 
 export default App
---
+++ Frontend/restaurant-application/src/pages/Success.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Success() {
+  return (
+    <div>
+      <h1>SUCCESS PAGE</h1>
+    </div>
+  )
+}
+
+export default Success
