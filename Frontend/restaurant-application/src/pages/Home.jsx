003de9e6258fb6ace96582115c01458cee4b3389
KavyaBajpai <kavyabajpai05@gmail.com>
2024-12-14 20:06:24 +0530

    added navbar and herosection components

--- Frontend/restaurant-application/src/pages/Home.jsx
+++ Frontend/restaurant-application/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import HeroSection from '../components/HeroSection'
 
 function Home() {
   return (
     <div>
       <h1> HOME PAGE </h1>
+      <HeroSection />
     </div>
   )
 }
