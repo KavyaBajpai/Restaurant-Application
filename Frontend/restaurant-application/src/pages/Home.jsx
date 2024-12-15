62863e1ea7f81786a7a37e8e6c9a00f8c2ea7a20
KavyaBajpai <kavyabajpai05@gmail.com>
2024-12-15 16:22:37 +0530

    added remaining components

--- Frontend/restaurant-application/src/pages/Home.jsx
+++ Frontend/restaurant-application/src/pages/Home.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import HeroSection from '../components/HeroSection'
 import About from '../components/About'
 import Qualities from '../components/Qualities'
+import Menu from '../components/Menu'
+import WhoAreWe from '../components/WhoAreWe'
+import Team from '../components/Team'
+import Footer from '../components/Footer'
+import Reservation from '../components/Reservation'
 
 function Home() {
   return (
@@ -9,6 +14,11 @@ function Home() {
       <HeroSection />
       <About />
       <Qualities />
+      <Menu />
+      <WhoAreWe />
+      <Team />
+      <Reservation />
+      <Footer />
     </div>
   )
 }
